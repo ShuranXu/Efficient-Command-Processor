bb838bee964f61462a3290cb8e781858fc50a308
ShuranXu <shuran.xu@mail.utoronto.ca>
2021-12-03 13:21:53 -0500

    Added source code implementation for the huffman coding

---
+++ huffman/huffman.h
@@ -0,0 +1,10 @@
+#ifndef __HUFFMAN_CODE_H__
+#define __HUFFMAN_CODE_H__
+
+void import_huffman_table();
+
+void encode_message(const char *s, char *out);
+
+void decode_message(const char *s);
+
+#endif /* __HUFFMAN_CODE_H__ */
